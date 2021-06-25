f118be17c9797beb68a488008a179394da27df72
Duy Anh <huynhduyanh123123@gmail.com>
2021-06-25 10:42:18 +0700

    bumped version

--- bukkit/src/test/java/configs/Market.java
+++ bukkit/src/test/java/configs/Market.java
@@ -1,9 +1,6 @@
 package configs;
 
-import dev.anhcraft.config.annotations.Configurable;
-import dev.anhcraft.config.annotations.Path;
-import dev.anhcraft.config.annotations.Setting;
-import dev.anhcraft.config.annotations.Validation;
+import dev.anhcraft.config.annotations.*;
 
 import java.util.List;
 import java.util.Map;
@@ -12,6 +9,29 @@ import java.util.Map;
 public class Market {
     @Setting
     @Path("items")
+    @Examples(
+            {
+                    @Example(
+                            {
+                                    "flags:",
+                                    "  '1':",
+                                    "    location: assault -345.41 43.00 -224.30 52.35 11.55",
+                                    "    display_name:",
+                                    "      valid: \"&a&l{__flag_team__} | &f&l{__flag_health__} &c&l❤\"",
+                                    "      invalid: \"&7&l{__flag_team__} | &f&l{__flag_health__}/{__flag_max_health__} &c&l❤\"",
+                                    "      neutral: \"&7Neutral\"",
+                                    "    max_health: 10",
+                                    "  '2':",
+                                    "    location: assault -355.86 40.50 -285.70 176.70 90.00",
+                                    "    display_name:",
+                                    "      valid: \"&a&l{__flag_team__} | &f&l{__flag_health__} &c&l❤\"",
+                                    "      invalid: \"&7&l{__flag_team__} | &f&l{__flag_health__}/{__flag_max_health__} &c&l❤\"",
+                                    "      neutral: \"&7Neutral\"",
+                                    "    max_health: 15"
+                            }
+                    )
+            }
+    )
     public Item[] products;
 
     @Setting
