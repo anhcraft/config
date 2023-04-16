2afbcb259b0c914f50b2d560fba1f0ed9f24d6d8
anhcraft <huynhduyanh123123@gmail.com>
2023-04-16 21:57:00 +0700

    Drop support below 1.12.2

--- bukkit/src/main/java/dev/anhcraft/config/bukkit/NMSVersion.java
+++ bukkit/src/main/java/dev/anhcraft/config/bukkit/NMSVersion.java
@@ -4,13 +4,6 @@ import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
 public enum NMSVersion {
-    v1_8_R1,
-    v1_8_R2,
-    v1_8_R3,
-    v1_9_R1,
-    v1_9_R2,
-    v1_10_R1,
-    v1_11_R1,
     v1_12_R1,
     v1_13_R1,
     v1_13_R2,
