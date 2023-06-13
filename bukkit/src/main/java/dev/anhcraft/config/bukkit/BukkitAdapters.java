78b16b1b55ae7a59c2e1b55e2d9d0e56464e70d4
anhcraft <huynhduyanh123123@gmail.com>
2023-06-13 19:57:44 +0700

    Fix serve adapter mistypes

--- bukkit/src/main/java/dev/anhcraft/config/bukkit/BukkitAdapters.java
+++ bukkit/src/main/java/dev/anhcraft/config/bukkit/BukkitAdapters.java
@@ -5,7 +5,10 @@ import dev.anhcraft.config.bukkit.adapters.*;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.block.banner.Pattern;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.util.BlockVector;
@@ -17,15 +20,15 @@ public class BukkitAdapters {
     public static void registerFor(@NotNull ConfigHandler handler) {
         handler.registerTypeAdapter(Location.class, new LocationAdapter());
         handler.registerTypeAdapter(Vector.class, new VectorAdapter());
-        handler.registerTypeAdapter(AttributeModifierAdapter.class, new AttributeModifierAdapter());
+        handler.registerTypeAdapter(AttributeModifier.class, new AttributeModifierAdapter());
         handler.registerTypeAdapter(BlockVector.class, new BlockVectorAdapter());
         handler.registerTypeAdapter(Pattern.class, new PatternAdapter());
         handler.registerTypeAdapter(BoundingBox.class, new BoundingBoxAdapter());
         handler.registerTypeAdapter(Color.class, new ColorAdapter());
         handler.registerTypeAdapter(PotionEffect.class, new PotionEffectAdapter());
         handler.registerTypeAdapter(FireworkEffect.class, new FireworkEffectAdapter());
-        handler.registerTypeAdapter(EnchantmentAdapter.class, new EnchantmentAdapter());
-        handler.registerTypeAdapter(NamespacedKeyAdapter.class, new NamespacedKeyAdapter());
+        handler.registerTypeAdapter(Enchantment.class, new EnchantmentAdapter());
+        handler.registerTypeAdapter(NamespacedKey.class, new NamespacedKeyAdapter());
         handler.registerTypeAdapter(ItemStack.class, new ItemStackAdapter());
     }
 }
