0fb4bf34062baca3a13d313adc7ce5a51e7362cd
anhcraft <huynhduyanh123123@gmail.com>
2024-02-26 18:22:55 +0700

    Improve ReflectBlueprintScannerTest

--- core/src/test/java/dev/anhcraft/config/blueprint/ReflectBlueprintScannerTest.java
+++ core/src/test/java/dev/anhcraft/config/blueprint/ReflectBlueprintScannerTest.java
@@ -3,10 +3,7 @@ package dev.anhcraft.config.blueprint;
 import dev.anhcraft.config.NamingStrategy;
 import dev.anhcraft.config.context.PathType;
 import dev.anhcraft.config.error.UnsupportedSchemaException;
-import dev.anhcraft.config.meta.Alias;
-import dev.anhcraft.config.meta.Describe;
-import dev.anhcraft.config.meta.Exclude;
-import dev.anhcraft.config.meta.Name;
+import dev.anhcraft.config.meta.*;
 import dev.anhcraft.config.type.TypeToken;
 import dev.anhcraft.config.validate.ValidationRegistry;
 import org.junit.jupiter.api.BeforeAll;
@@ -38,23 +35,43 @@ public class ReflectBlueprintScannerTest {
     }
 
     @Nested
-    public class ModelTest {
+    public class SimpleModelTest {
+        private final Schema schema = scanner.scanSchema(Profile.class);
+
+        @Test
+        public void testInit() {
+            assertEquals(Profile.class, schema.type());
+            assertEquals(4, schema.properties().size());
+            assertEquals(Set.of("id", "email", "age", "birth", "bio"), schema.propertyNames());
+        }
+
         @SuppressWarnings("DataFlowIssue")
         @Test
-        public void testScan() {
-            Schema schema = scanner.scanSchema(Profile.class);
-            assertEquals(Profile.class, schema.type());
-            assertEquals(2, schema.properties().size());
-            assertEquals(Set.of("id", "age", "birth"), schema.propertyNames());
-
+        public void testUser() {
             assertEquals("id", schema.property("id").name());
             assertEquals("user", schema.property("id").field().getName());
             assertTrue(schema.property("id").description().isEmpty());
             assertTrue(schema.property("id").aliases().isEmpty());
-            assertFalse(schema.property("id").isConstant());
+            assertTrue(schema.property("id").isConstant());
             assertFalse(schema.property("id").isTransient());
             assertFalse(schema.property("id").isOptional());
+        }
 
+        @SuppressWarnings("DataFlowIssue")
+        @Test
+        public void testEmail() {
+            assertEquals("email", schema.property("email").field().getName());
+            assertTrue(schema.property("email").description().isEmpty());
+            assertTrue(schema.property("email").aliases().isEmpty());
+            assertFalse(schema.property("email").isConstant());
+            assertFalse(schema.property("email").isTransient());
+            assertFalse(schema.property("email").isOptional());
+            assertFalse(schema.property("email").validator().check(null));
+        }
+
+        @SuppressWarnings("DataFlowIssue")
+        @Test
+        public void testAge() {
             assertEquals("age", schema.property("age").name());
             assertEquals("age", schema.property("age").field().getName());
             assertEquals(List.of("Age in years"), schema.property("age").description());
@@ -72,16 +89,37 @@ public class ReflectBlueprintScannerTest {
             assertFalse(schema.property("birth").isOptional());
         }
 
+        @SuppressWarnings("DataFlowIssue")
+        @Test
+        public void testBio() {
+            assertEquals("bio", schema.property("bio").field().getName());
+            assertTrue(schema.property("bio").description().isEmpty());
+            assertTrue(schema.property("bio").aliases().isEmpty());
+            assertTrue(schema.property("bio").isTransient());
+            assertTrue(schema.property("bio").isOptional());
+            assertFalse(schema.property("bio").isConstant());
+        }
+
         public class Profile {
             @Alias("birth")
             @Describe("Age in years")
             public final int age = 18;
+
             @Name("id")
+            @Constant
             public String user;
+
+            @Validate("notNull")
+            public String email;
+
             public transient double balance;
 
             @Exclude
             public List<String> education;
+
+            @Optional
+            @Transient
+            public List<String> bio = List.of();
         }
     }
 }
