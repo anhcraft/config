70b48c431ead5e46853e821436bcdde5c6d929d0
anhcraft <huynhduyanh123123@gmail.com>
2024-12-07 14:03:07 +0700

    fix(core): module-info missed exporting nested packages

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,14 +1,16 @@
 module dev.anhcraft.config {
   exports dev.anhcraft.config;
   exports dev.anhcraft.config.adapter;
+  exports dev.anhcraft.config.adapter.defaults;
   exports dev.anhcraft.config.blueprint;
   exports dev.anhcraft.config.context;
+  exports dev.anhcraft.config.context.injector;
   exports dev.anhcraft.config.error;
   exports dev.anhcraft.config.meta;
   exports dev.anhcraft.config.type;
   exports dev.anhcraft.config.util;
   exports dev.anhcraft.config.validate;
-  exports dev.anhcraft.config.internal.blueprint;
+  exports dev.anhcraft.config.validate.check;
 
   requires jdk.unsupported;
   requires org.jetbrains.annotations;
